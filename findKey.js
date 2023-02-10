08e36690884b67483052bf5a2c1760d371992033
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-09 16:39:59 -0800

    find key

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKey = (object, callback) => {
+  let arr = Object.keys(object);
+  for (let i of arr) {
+    let res = callback(object[i]);
+    if (res === true) {
+      return i;
+    }
+  }
+  return;
+};
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
