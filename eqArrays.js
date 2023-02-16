35fce36b0b5e7eb3d769b4626fc467f0f22255fe
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-15 19:09:06 -0800

    add recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,21 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
     for (let i in arr1) {
-      if (arr1[i] !== arr2[i]) {
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        let res = eqArrays(arr1[i], arr2[i]);
+        if (!res) {
+          return false;
+        }
+      } else if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
@@ -18,3 +23,16 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  ),
+  false
+);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
