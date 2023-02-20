05648e7cd912423e5feefffc1e213c3407e6820d
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-02-20 11:22:29 -0800

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
@@ -22,17 +15,4 @@ const eqArrays = function(arr1, arr2) {
     return true;
   }
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-
-assertEqual(
-  eqArrays(
-    [[2, 3], [4]],
-    [
-      [2, 3],
-      [4, 5],
-    ]
-  ),
-  false
-);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+module.exports = eqArrays;
